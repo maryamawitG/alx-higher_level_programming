6d2a0f94d7901b82684cd1749008e4382913df59
maryamawitG <maryamwit4kidus@gmail.com>
2023-03-27 01:42:55 -0700

    3. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter not in [101,113]:
-                print("{}".format(letter))
-
+    if letter not in [101, 113]:
+        print("{}".format(letter))
