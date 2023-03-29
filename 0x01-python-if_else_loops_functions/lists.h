d0003c3fca0a200c225644ab15cd47e318690907
maryamawitG <maryamwit4kidus@gmail.com>
2023-03-29 02:36:48 -0700

    8. To uppercase

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,6 +1,15 @@
 #ifdef LISTS_H
 #define LISTS_H
 def islower(c);
+def uppercase(str):
+def print_last_digit(number):
+def add(a, b):
+def pow(a, b):
+def fizzbuzz():
+listint_t *insert_node(listint_t **head, int number);
+def remove_char_at(str, n):
+def magic_calculation(a, b, c):
+
 
 #endif
 
